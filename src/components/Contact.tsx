e604c2863e585c5092726583ceec85168aff121a
nadhil7 <nadhil.devxtra@gmail.com>
2025-10-19 17:06:41 +0530

    contact done

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,19 +1,22 @@
+import React, { useRef } from 'react';
 import { motion } from "framer-motion";
+import emailjs from 'emailjs-com';
 import black from "../Pictures/black.jpeg";
 import white from "../Pictures/white.jpg";
 
 export default function Contact() {
+
   const cakeImages = [
-    {image:"https://images.unsplash.com/photo-1578985545062-69928b1d9587?auto=format&fit=crop&w=600&q=80"},
-    {image:black},
-    {image:"https://images.unsplash.com/photo-1509440159596-0249088772ff?auto=format&fit=crop&w=600&q=80"},
-    {image:white},
+    { image: "https://images.unsplash.com/photo-1578985545062-69928b1d9587?auto=format&fit=crop&w=600&q=80" },
+    { image: black },
+    { image: "https://images.unsplash.com/photo-1509440159596-0249088772ff?auto=format&fit=crop&w=600&q=80" },
+    { image: white },
   ];
 
   return (
     <section id="contact" className="py-20 px-4 bg-gradient-to-b from-pink-50 via-white to-yellow-50">
       <div className="max-w-7xl mx-auto flex flex-col lg:flex-row items-center gap-16">
-        
+
         {/* Left Side - Contact Form */}
         <div className="flex-1 w-full">
           <div className="text-center lg:text-left mb-8 lg:mb-0 lg:pl-20">
@@ -21,23 +24,29 @@ export default function Contact() {
               Contact Us
             </h2>
             <p className="text-gray-600 mb-8 text-lg">
-              Reach out to us: <span className="font-medium text-pink-600">cakefairy1@gmail.com</span>
+              Reach out to us: <span className="font-medium text-pink-600">watsapp</span>
             </p>
           </div>
 
           <form className="max-w-md mx-auto flex flex-col gap-6">
             <input
               type="text"
+              name="user_name"
               placeholder="Name"
+              required
               className="border border-gray-300 rounded-xl p-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-pink-400 focus:border-pink-400 transition"
             />
             <input
               type="text"
+              name="user_phone"
               placeholder="Phone"
+              required
               className="border border-gray-300 rounded-xl p-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-pink-400 focus:border-pink-400 transition"
             />
             <textarea
+              name="message"
               placeholder="Type your requirements"
+              required
               className="border border-gray-300 rounded-xl p-4 shadow-sm focus:outline-none focus:ring-2 focus:ring-pink-400 focus:border-pink-400 transition resize-none h-32"
             />
             <button
